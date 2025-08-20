70da02496893863b3849ffded5ccff5391599567
Maaiz Shaikh <88193033+Maaiz-Shaikh@users.noreply.github.com>
2025-08-20 22:58:49 +0530

    Create Kadane's Algorithm.java

---
+++ Arrays/FAQs(Medium)/Kadane's Algorithm.java
@@ -0,0 +1,16 @@
+// Approach 2: Kadane's Algorithm
+// TC: O(n) SC: O(1)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        
+        for(int i=0; i<nums.length; i++) {
+            sum+=nums[i];
+            maxSum = Math.max(maxSum, sum);
+            if(sum < 0) sum = 0;
+        }
+
+        return maxSum;
+    }
+}
