a2f0b01c8ff8710699d86483c01aaad917b341b3
Maaiz Shaikh <88193033+Maaiz-Shaikh@users.noreply.github.com>
2025-08-20 23:08:00 +0530

    Update Kadane's Algorithm.java

--- Arrays/FAQs(Medium)/Kadane's Algorithm.java
+++ Arrays/FAQs(Medium)/Kadane's Algorithm.java
@@ -14,3 +14,29 @@ class Solution {
         return maxSum;
     }
 }
+
+
+// Follow up question: Print longest sum subarray
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        int startInd = 0;
+        int endInd = 0;
+        
+        for(int i=0; i<nums.length; i++) {
+            if(sum == 0) startInd = i;
+            sum+=nums[i];
+            if(sum > maxSum) {
+                maxSum = sum;
+                endInd = i;
+            }
+
+            if(sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+}
